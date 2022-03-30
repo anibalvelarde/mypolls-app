138acea7715e7d148ac64c2c1c0a9457788a44b9
AEV0129 <AEV0129@daugherty.com>
2022-03-29 19:05:16 -0500

    updated the login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,8 +8,17 @@ import {
 import { Box } from "@mui/system";
 import React from "react";
 import Center from "./Center";
+import useForm from "../hooks/useForm";
+
+const getFreshModel = () => ({
+  name: "",
+  email: "",
+});
 
 export default function Login() {
+  const { values, errors, setErrors, handleInputChange } =
+    useForm(getFreshModel);
+
   return (
     <Center>
       <Card sx={{ width: "400px" }}>
@@ -26,8 +35,20 @@ export default function Login() {
             }}
           >
             <form noValidate autoComplete="off">
-              <TextField label="Email" name="email" variant="outlined" />
-              <TextField label="Name" name="name" variant="outlined" />
+              <TextField
+                label="Email"
+                name="email"
+                value={values.email}
+                onChange={handleInputChange}
+                variant="outlined"
+              />
+              <TextField
+                label="Name"
+                name="name"
+                value={values.name}
+                onChange={handleInputChange}
+                variant="outlined"
+              />
               <Button
                 type="submit"
                 variant="contained"
